105faf5cb7428494f6bad5c7253c0f6b10708573
milypol98 <milypol98@gmail.com>
2022-09-04 11:32:04 +0200

    FirstCommit

---
+++ src/test/java/pl/milypol/pizzadelivery/PizzaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milypol.pizzadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
